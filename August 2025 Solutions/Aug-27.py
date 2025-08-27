f933f750b67d37e033112f4920410bba8d55c36b
Amit Khotele <142614018+amitkhotele@users.noreply.github.com>
2025-08-27 11:45:02 +0530

    Create Aug-27.py

---
+++ August 2025 Solutions/Aug-27.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countTriangles(self, arr):
+        n = len(arr)
+        arr.sort()
+        count = 0
+
+        # Fix the largest element one by one
+        for k in range(n - 1, 1, -1):
+            i = 0
+            j = k - 1
+            while i < j:
+                if arr[i] + arr[j] > arr[k]:
+                    # all elements between i and j form valid triangles
+                    count += (j - i)
+                    j -= 1
+                else:
+                    i += 1
+        return count
